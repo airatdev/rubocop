4e49db5c2ae4b17c991c6d16f064bcf89f8efcbd
Airat Shigapov <hi@airat.sh>
2018-04-23 14:52:40 +0300

    Draft new cop for checking hash elements in array

---
+++ spec/rubocop/cop/style/braces_around_last_hash_in_array_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Style::BracesAroundLastHashInArray, :config do
+  subject(:cop) { described_class.new(config) }
+
+  shared_examples 'single line lists' do |extra_info|
+    it 'accepts empty literal' do
+      expect_no_offenses('VALUES = []')
+    end
+  end
+
+  context 'with single line list of values' do
+    context 'when EnforcedStyle is no_braces' do
+      let(:cop_config) { { 'EnforcedStyleForMultiline' => 'no_comma' } }
+
+      include_examples 'single line lists', ''
+    end
+  end
+end
